d622d86831b6281289ee29545a2c4d9e0e523e14
unknown-user <unknown-user@192.168.20.38>
2015-02-27 07:45:28 -0300

    Verificação inicial do módulo MUDSolution

---
+++ MUDConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MUDConsole
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+
+			string x = Console.ReadLine ();
+
+			Console.WriteLine (x);
+		}
+	}
+}
